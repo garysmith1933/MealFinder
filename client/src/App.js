718d1087a183f96b9af3439acb0cd3d478ef80e1
Gary Smith <garysmith193358@gmail.com>
2022-08-11 20:50:22 -0400

    food api set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+console.log(process.env.REACT_APP_KEY)
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is the main page
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,10 +1,25 @@
 
+require('dotenv').config();
 const express = require("express");
-
+const fetch = require('node-fetch');
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
+app.get('/data', async (req,res,next) => {
+    try {
+        const apiKey = `apiKey=${process.env.REACT_APP_KEY}`
+        const api = "https://api.spoonacular.com/recipes/findByIngredients?"
+        const ingredients = "&ingredients=chicken"
+        const url = api + apiKey + ingredients
+        const data = await fetch(url)
+        const json = await data.json()
+        res.json(json)
+    } 
+    
+    catch (err) {
+        next(err)
+    }
+})
 
 app.get('/', (req,res,next) => {
     try {
