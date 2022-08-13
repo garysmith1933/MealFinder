22e9a240bc55c9533bc05cb972061b1cf9b0444c
Gary Smith <garysmith193358@gmail.com>
2022-08-13 18:58:33 -0400

    able to display api data on front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,52 @@
-
+import {getRecipes} from './store/store.js'
 import './App.css';
+import {connect} from 'react-redux'
+import {useState, useEffect} from 'react'
 
-function App() {
+
+function App({getRecipes,state}) {
+  const [query, setQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState('')
+  const [results, setResults] = useState(null)
+
+  const searchResults = () => {
+    setSearchQuery(query)
+    getRecipes(query)
+  }
+
+  useEffect(() => {
+  //pick off from here
+  console.log(state)
+    setResults(state[0])
+  },[state])
+
+  const recipeInfo = results ? 
+  results.length < 1 ? `No Results for ${searchQuery}` :
+  results.map(recipe => {
+    return <div key={recipe.id}>{recipe.title}</div>
+  }) : null
+
   return (
     <div className="App">
         <h1>
           What do you want for dinner?
         </h1>
 
-        <input type='text' placeholder="ex chicken"/>
-        <button onClick={() => console.log('chicken')}> Submit </button> 
+        <input type='text' placeholder="ex chicken" onChange={(ev) => setQuery(ev.target.value)}/>
+        <button onClick={() => searchResults()}> Submit </button> 
+
+        <div> {recipeInfo} </div>
     </div>
   );
 }
 
-export default App;
+const mapState = (state) => {
+  return {state}
+}
+
+const mapDispatch = (dispatch) => {
+  return {
+    getRecipes: async(query)=> await dispatch(getRecipes(query))
+  }
+}
+export default connect(mapState, mapDispatch)(App);
--- server/index.js
+++ server/index.js
@@ -5,15 +5,20 @@ const fetch = require('node-fetch');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.get('/foodData', async (req,res,next) => {
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/recipeResults', async (req,res,next) => {
     try {
         const apiKey = `apiKey=${process.env.REACT_APP_KEY}`
         const api = "https://api.spoonacular.com/recipes/findByIngredients?"
-        const ingredients = "&ingredients=chicken"
-        const url = api + apiKey + ingredients
+        const ingredients = `&ingredients=${req.body.ingredients}`
+        const number = '&number=1'
+        console.log(ingredients)
+        const url = api + apiKey + ingredients + number
         const data = await fetch(url)
         const json = await data.json()
-        res.json(json)
+        res.send(json)
     } 
     
     catch (err) {
