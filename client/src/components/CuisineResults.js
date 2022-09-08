457a99e6e1510ec2035551f38e6c138cf879683d
Gary Smith <garysmith193358@gmail.com>
2022-09-08 00:48:21 -0400

    user can now search based on cuisine

--- client/src/components/CuisineResults.js
+++ client/src/components/CuisineResults.js
@@ -2,31 +2,42 @@ import { useLocation } from 'react-router-dom'
 import { useEffect } from 'react'
 import { getCuisineRecipes } from '../store/cuisines'
 import {connect} from 'react-redux'
+import {addToStorage} from "../storage.js"
 
-
-const CuisineResults = (props) => {
-    const {results} = useLocation().state
-
+const CuisineResults = ({getCuisineRecipes, cuisines}) => {
+    const {cuisineName} = useLocation().state
+    
     useEffect(() => {
-       props.getCuisineRecipes(results)
+      getCuisineRecipes(cuisineName)
+      console.log(cuisines)
     },[])
+
+    const recipeInfo = cuisines.results ? 
+    cuisines.results.map(recipe => {
+        return <div className='recipes' key={recipe.id}>
+            <h5 className='recipe-title'> {recipe.title} </h5>
+                <div className='recipe-body'> 
+                    <div className='button' onClick={() => addToStorage(recipe.title, recipe.sourceUrl, recipe.image)}> + </div>
+                    <a href={`${recipe.sourceUrl}`} target="_blank">
+                        <div className='recipe-image' style={{backgroundImage: `url(${recipe.image})`}}></div>
+                    </a>
+                    <div className='button'> - </div>
+                </div>  
+        </div>
+  }) : null
 
     return (
         <>
-        <h1> Results for Cuisine </h1>
-            {/* {cuisineRecipes.map(recipe => {
-                return (
-                    <div>
-                        {recipe.name}
-                    </div>
-                )
-            })} */}
+        <h1> {cuisineName} Cuisine </h1>
+          <div>{recipeInfo}</div>
         </>   
     )
 }
 
 const mapState = (state) => {
-  return {cuisineRecipes: state.cuisineRecipes};
+  console.log(state)
+  const {cuisines} = state;
+  return {cuisines}
 }
 const mapDispatch = (dispatch) => {
   return {
