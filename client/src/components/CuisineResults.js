1a391f2c898e302eefcd481f230275340fb0e800
Gary Smith <garysmith193358@gmail.com>
2022-09-10 13:32:12 -0400

    cuisine recipes results now have pagination

--- client/src/components/CuisineResults.js
+++ client/src/components/CuisineResults.js
@@ -1,35 +1,66 @@
 import { useLocation } from 'react-router-dom'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { getCuisineRecipes } from '../store/cuisines'
 import {connect} from 'react-redux'
 import {addToStorage} from "../storage.js"
+import {Pagination, Grid} from "@mui/material"
+
+
+const pageSize = 3;
 
 const CuisineResults = ({getCuisineRecipes, cuisines}) => {
     const {cuisineName} = useLocation().state
+    const [currentRecipes, setCurrentRecipes] = useState(cuisines.results)
+    const [pagination, setPagination] = useState({
+      count: 0,
+      start: 0,
+      end: pageSize
+    })
     
     useEffect(() => {
       getCuisineRecipes(cuisineName)
       console.log(cuisines)
     },[])
 
-    const recipeInfo = cuisines.results ? 
-    cuisines.results.map(recipe => {
-        return <div className='recipes' key={recipe.id}>
-            <h5 className='recipe-title'> {recipe.title} </h5>
-                <div className='recipe-body'> 
+    useEffect(() => {
+      if(cuisines.results) {
+        setPagination({...pagination, count:cuisines.results.length})
+        setCurrentRecipes(cuisines.results.slice(pagination.start,pagination.end))
+      }
+      
+    }, [cuisines, pagination.start, pagination.end])
+
+    
+    const handlePageChange = (event, page) => {
+      const newStart = (page-1) * pageSize
+      const newEnd = (page-1) * pageSize + pageSize
+      setPagination({
+        ...pagination,
+        start: newStart,
+        end: newEnd
+      })
+    }
+
+    const recipeInfo = currentRecipes ? 
+      currentRecipes.map(recipe => {
+      return <div className='cusine-recipes' key={recipe.id}>
+                <h5 className='recipe-title'> {recipe.title} </h5>
+
+                <div className='cuisine-body'> 
                     <div className='button' onClick={() => addToStorage(recipe.title, recipe.sourceUrl, recipe.image)}> + </div>
-                    <a href={`${recipe.sourceUrl}`} target="_blank">
+                      <a href={`${recipe.sourceUrl}`} target="_blank">
                         <div className='recipe-image' style={{backgroundImage: `url(${recipe.image})`}}></div>
-                    </a>
+                      </a>
                     <div className='button'> - </div>
-                </div>  
-        </div>
-  }) : null
+                  </div>  
+              </div>
+    }) : null
 
     return (
         <>
-        <h1> {cuisineName} Cuisine </h1>
+          <h1> {cuisineName} Cuisine </h1>
           <div>{recipeInfo}</div>
+          <Pagination count={Math.ceil(pagination.count/pageSize)} onChange={handlePageChange} />
         </>   
     )
 }
