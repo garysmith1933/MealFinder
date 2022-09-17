0f282eecb4cfcde827b466ef1bb8e3c40f7cc3f8
Gary Smith <garysmith193358@gmail.com>
2022-09-17 13:39:33 -0400

    a

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.post('/recipeResults', async (req,res,next) => {
     try {
