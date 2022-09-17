62b07309651c3b4e036587e110b4346ba605fb30
Gary Smith <garysmith193358@gmail.com>
2022-09-17 15:23:05 -0400

    deploy

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const path = require('path')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('build'));
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.post('/recipeResults', async (req,res,next) => {
