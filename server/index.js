49a9bacd6cb091b1a20b2c52c047efb41b56b3f4
Gary Smith <garysmith193358@gmail.com>
2022-09-17 15:25:43 -0400

    heroku

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const fetch = require('node-fetch');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const path = require('path')
+const path = require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
