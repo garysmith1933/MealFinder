ee127a3fae3fe3cd2f15936269c562c01dc47a6f
Gary Smith <garysmith193358@gmail.com>
2022-09-19 09:38:38 -0400

    another one

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const path = require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '..', 'client/build')));
 
 
 app.post('/recipeResults', async (req,res,next) => {
