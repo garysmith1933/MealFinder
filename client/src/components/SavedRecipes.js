7c5a2310c978ff0ebcb529d67e2fcc503c62b7c8
Gary Smith <garysmith193358@gmail.com>
2022-11-16 15:32:02 -0500

    fixed bug that shows empty page when savedRecipes in local storage is not initalized

--- client/src/components/SavedRecipes.js
+++ client/src/components/SavedRecipes.js
@@ -6,11 +6,12 @@ let results = JSON.parse(window.localStorage.getItem("savedRecipes"))
 
   return (
     <>
-      {results.length > 0 ? <PaginatedResults recipes={results} query={'Your Saved Recipes'}/> : 
-      
-      <div className='results' style={{display:'flex', justifyContent:'center'}}> 
+      {!results || results.length > 0 ? 
+       <div className='results' style={{display:'flex', justifyContent:'center'}}> 
         <h1>You have no saved recipes</h1>
-      </div>}
+      </div> :
+      
+      <PaginatedResults recipes={results} query={'Your Saved Recipes'}/>}
     </>   
   );
 }
