e6341d68510ac9394af799ec9b4c46d80331d7ea
Gary Smith <garysmith193358@gmail.com>
2023-03-21 18:32:41 -0400

    improved formatting of single recipe component

--- client/src/components/SingleRecipe.js
+++ client/src/components/SingleRecipe.js
@@ -5,15 +5,17 @@ export default function SingleRecipe({title, image, url}) {
     title = title.length > 30 ? `${title.slice(0,30)}...` : title
     return (
         <div className='single-recipe'>
-                <Card sx={{maxHeight:400, minHeight: 400, maxWidth: 400}} className='image'>
-                    <a href={url} alt={title} target='_blank'>
-                        <CardMedia component='img' image={image} alt={title} sx={{height: 300}}/>  
-                    </a>
-                    <CardContent>
-                        <Typography variant='h6'>{title}</Typography>
-                        <button className='save-button button' onClick={() => addToStorage(title, url, image)}>Save Recipe</button>
-                    </CardContent>
-                </Card>
+          <Card sx={{maxHeight:400, minHeight: 400, maxWidth: 400}} className='image'>
+            <a href={url} alt={title} target='_blank'>
+              <CardMedia component='img' image={image} alt={title} sx={{height: 300}}/>  
+            </a>
+
+            <CardContent>
+              <Typography variant='h6'>{title}</Typography>
+              <button className='save-button button' onClick={() => addToStorage(title, url, image)}>Save Recipe</button>
+            </CardContent>
+            
+          </Card>
         </div>
     )
 }
