70853600c8aaff6e64543f6c644f5f5425781f51
Gary Smith <garysmith193358@gmail.com>
2023-03-22 16:42:48 -0400

    improved code formatting on paginated results component

--- client/src/components/PaginatedResults.js
+++ client/src/components/PaginatedResults.js
@@ -7,47 +7,49 @@ import SingleSavedRecipe from './SingleSavedRecipe'
 const pageSize = 8;
 
 const PaginatedResults = (props) => {
-    const {recipes, query} = useLocation().state || props
-    const [currentRecipes, setCurrentRecipes] = useState(recipes)
-    const [pagination, setPagination] = useState({
-      count: 0,
-      start: 0,
-      end: pageSize
-    })
+  const {recipes, query} = useLocation().state || props
+  const [currentRecipes, setCurrentRecipes] = useState(recipes)
+  const [pagination, setPagination] = useState({
+    count: 0,
+    start: 0,
+    end: pageSize
+  })
 
-    const pageTitle = query === 'Your Saved Recipes' ? query : `Results for ${query}`
+  const pageTitle = query === 'Your Saved Recipes' ? query : `Results for ${query}`
   
-    useEffect(() => {
-        setPagination({...pagination, count:recipes.length})
-        setCurrentRecipes(recipes.slice(pagination.start,pagination.end))
-    }, [recipes, pagination.start, pagination.end])
+  useEffect(() => {
+    setPagination({...pagination, count:recipes.length})
+    setCurrentRecipes(recipes.slice(pagination.start,pagination.end))
+  }, [recipes, pagination.start, pagination.end])
 
-    const handlePageChange = (event, page) => {
-      const newStart = (page-1) * pageSize
-      const newEnd = (page-1) * pageSize + pageSize
-      setPagination({
-        ...pagination,
-        start: newStart,
-        end: newEnd
-      })
-    }
+  const handlePageChange = (event, page) => {
+    const newStart = (page-1) * pageSize
+    const newEnd = (page-1) * pageSize + pageSize
+    setPagination({
+      ...pagination,
+      start: newStart,
+      end: newEnd
+    })
+  }
 
-    const recipeInfo = currentRecipes ? 
-      currentRecipes.map(recipe => (
-        <Grid item xs={12} sm={6} md={3} key={recipe.title} justifyContent='center'>
+  const recipeInfo = currentRecipes ? 
+    currentRecipes.map(recipe => (
+      <Grid item xs={12} sm={6} md={3} key={recipe.title} justifyContent='center'>
         {query === 'Your Saved Recipes' ? <SingleSavedRecipe title={recipe.title} url={recipe.sourceUrl} image={recipe.image}/> :
-          <SingleRecipe title={recipe.title} url={recipe.sourceUrl} image={recipe.image}/>}
-        </Grid>
+        <SingleRecipe title={recipe.title} url={recipe.sourceUrl} image={recipe.image}/>}
+      </Grid>
       )) : null
 
-    return (
-        <div className='results'>
-          <h1>{pageTitle}</h1>
-          <Grid container spacing={{xs:2, md:3}}>
-            {recipeInfo}
-          </Grid>
-          <Pagination sx={{display: 'flex', justifyContent:'center', marginTop: '1.5rem'}}count={Math.ceil(pagination.count/pageSize)} onChange={handlePageChange} />
-        </div>   
+  return (
+    <div className='results'>
+      <h1>{pageTitle}</h1>
+
+      <Grid container spacing={{xs:2, md:3}}>
+        {recipeInfo}
+      </Grid>
+
+      <Pagination sx={{display: 'flex', justifyContent:'center', marginTop: '1.5rem'}}count={Math.ceil(pagination.count/pageSize)} onChange={handlePageChange} />
+    </div>   
     )
 }
 
